32e19d10aa9fb89f8d125b2c115ce69a9343cc71
GustavsLieknins <126756140+GustavsLieknins@users.noreply.github.com>
2025-01-24 10:18:05 +0200

    added

--- Desktop/react-app/react-vercel/src/index.js
+++ Desktop/react-app/react-vercel/src/index.js
@@ -8,25 +8,42 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <p style={{
-      textAlign: 'center',
-      fontSize: '1.5em',
-      margin: '30px 0',
-      color: '#4A90E2',
-      backgroundColor: '#F5F5F5',
-      borderRadius: '10px',
-      padding: '20px',
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-      lineHeight: '1.6'
+    <div className="main-container" style={{ 
+      backgroundImage: "linear-gradient(to right, #ffb347, #ffcc33)", 
+      border: "5px dashed black",
+      borderRadius: "15px",
+      padding: "20px",
+      transform: "rotate(5deg)"
     }}>
-      Kas ir prasības? Prasības ir izpratnes par to, ko darbinieks vai organizācija
-      vēlas panākt, lai uzlabotu savu darbību vai pakalpojumu. <br /><br />
-      Kas ir prasību specifikācija? Prasību specifikācija ir detalizēta informācija
-      par prasībām, kas tiek izmantotas, lai izveidotu vai uzlabotu
-      sistēmu, produktu vai pakalpojumu. <br /><br />
-      Un ar ko viena atšķirās no otras? Katra prasība ir unikāla un atšķiras no
-      citām ar savu saturu, mērķi un prioritāti.
-    </p>
+      <p className="main-text" style={{ 
+        fontSize: "25px", 
+        fontFamily: "Comic Sans MS", 
+        color: "white", 
+        textShadow: "2px 2px black"
+      }}>
+        Kas ir prasības? Prasības ir izpratnes par to, ko darbinieks vai organizācija
+        vēlas panākt, lai uzlabotu savu darbību vai pakalpojumu.
+      </p>
+      <p className="main-text" style={{ 
+        fontSize: "25px", 
+        fontFamily: "Comic Sans MS", 
+        color: "white", 
+        textShadow: "2px 2px black"
+      }}>
+        Kas ir prasību specifikācija? Prasību specifikācija ir detalizēta informācija
+        par prasībām, kas tiek izmantotas, lai izveidotu vai uzlabotu
+        sistēmu, produktu vai pakalpojumu.
+      </p>
+      <p className="main-text" style={{ 
+        fontSize: "25px", 
+        fontFamily: "Comic Sans MS", 
+        color: "white", 
+        textShadow: "2px 2px black"
+      }}>
+        Un ar ko viena atšķirās no otras? Katra prasība ir unikāla un atšķiras no
+        citām ar savu saturu, mērķi un prioritāti.
+      </p>
+    </div>
   </React.StrictMode>
 );
 
@@ -34,3 +51,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
